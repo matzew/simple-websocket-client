f076b07cbe631440e5df96e5cfa574c778146383
Matthias Wessendorf <matzew@apache.org>
2014-09-04 14:19:25 +0200

    Improving tests

--- src/test/java/net/wessendorf/websocket/AbstractSimpleClientTest.java
+++ src/test/java/net/wessendorf/websocket/AbstractSimpleClientTest.java
@@ -22,6 +22,7 @@ import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,10 +34,17 @@ public abstract class AbstractSimpleClientTest {
         final URI securedEndpointURL = new URI("ws://localhost:9999/echo");
         final SimpleWebSocketClient spc = new SimpleWebSocketClient(securedEndpointURL);
 
+
+        final AtomicReference<ByteBuffer> receivedPayload = new AtomicReference<ByteBuffer>(ByteBuffer.allocate(128));
+        final AtomicReference<ReadyState> receivedOpenReadyState = new AtomicReference<ReadyState>();
+        final AtomicReference<ReadyState> receivedCloseReadyState = new AtomicReference<ReadyState>();
+        final AtomicReference<Integer> receivedCloseCode = new AtomicReference<Integer>();
+
         spc.setWebSocketHandler(new WebSocketHandlerAdapter() {
             @Override
             public void onOpen() {
-                assertThat(spc.getReadyState()).isEqualTo(ReadyState.OPEN);
+                // store readystate on open:
+                receivedOpenReadyState.set(spc.getReadyState());
 
                 // create some bogus binary object...
                 ByteBuffer bb = ByteBuffer.allocate(128);
@@ -44,7 +52,6 @@ public abstract class AbstractSimpleClientTest {
                 bb.putChar('V');
                 bb.putChar('B');
 
-                // hrm.........
                 bb.flip();
 
                 spc.sendBinary(bb); // ship it!
@@ -52,8 +59,9 @@ public abstract class AbstractSimpleClientTest {
 
             @Override
             public void onClose(int closeCode, String reason) {
-                assertThat(spc.getReadyState()).isEqualTo(ReadyState.CLOSED);
-                assertThat(closeCode).isEqualTo(1000);
+                // store received data one close:
+                receivedCloseReadyState.set(spc.getReadyState());
+                receivedCloseCode.set(closeCode);
 
                 // cause the shutdown
                 closeLatch.countDown();
@@ -62,9 +70,12 @@ public abstract class AbstractSimpleClientTest {
             @Override
             public void onMessage(ByteBuffer message) {
 
-                assertThat(message.getChar()).isEqualTo('B');
-                assertThat(message.getChar()).isEqualTo('V');
-                assertThat(message.getChar()).isEqualTo('B');
+                receivedPayload.get().putChar(message.getChar());
+                receivedPayload.get().putChar(message.getChar());
+                receivedPayload.get().putChar(message.getChar());
+
+                // hrm
+                receivedPayload.get().flip();
 
                 // close it now:
                 spc.close();
@@ -76,34 +87,51 @@ public abstract class AbstractSimpleClientTest {
 
         // wait until the close was called
         closeLatch.await(2000, TimeUnit.MILLISECONDS);
+
+
+        assertThat(receivedOpenReadyState.get()).isEqualTo(ReadyState.OPEN);
+
+        assertThat(receivedPayload.get().getChar()).isEqualTo('B');
+        assertThat(receivedPayload.get().getChar()).isEqualTo('V');
+        assertThat(receivedPayload.get().getChar()).isEqualTo('B');
+
+        assertThat(receivedCloseReadyState.get()).isEqualTo(ReadyState.CLOSED);
+        assertThat(receivedCloseCode.get()).isEqualTo(1000);
     }
 
-    @Test
+   @Test
     public void simpleTextEcho() throws InterruptedException, URISyntaxException {
         final CountDownLatch closeLatch = new CountDownLatch(1);
         final URI securedEndpointURL = new URI("ws://localhost:9999/echo");
         final SimpleWebSocketClient spc = new SimpleWebSocketClient(securedEndpointURL);
 
+
+        final AtomicReference<String> receivedPayload = new AtomicReference<String>("");
+        final AtomicReference<ReadyState> receivedOpenReadyState = new AtomicReference<ReadyState>();
+        final AtomicReference<ReadyState> receivedCloseReadyState = new AtomicReference<ReadyState>();
+        final AtomicReference<Integer> receivedCloseCode = new AtomicReference<Integer>();
+
         spc.setWebSocketHandler(new WebSocketHandlerAdapter() {
             @Override
             public void onOpen() {
-                assertThat(spc.getReadyState()).isEqualTo(ReadyState.OPEN);
+                // store readystate on open:
+                receivedOpenReadyState.set(spc.getReadyState());
 
                 spc.sendText("Hello"); // ship it!
             }
 
             @Override
             public void onClose(int closeCode, String reason) {
-                assertThat(spc.getReadyState()).isEqualTo(ReadyState.CLOSED);
-                assertThat(closeCode).isEqualTo(1000);
+                // store received data one close:
+                receivedCloseReadyState.set(spc.getReadyState());
+                receivedCloseCode.set(closeCode);
                 // cause the shutdown
                 closeLatch.countDown();
             }
 
             @Override
             public void onMessage(String message) {
-
-                assertThat(message).isEqualTo("Hello");
+                receivedPayload.set(message);
                 // close it now:
                 spc.close();
             }
@@ -113,6 +141,14 @@ public abstract class AbstractSimpleClientTest {
 
         // wait until the close was called
         closeLatch.await(2000, TimeUnit.MILLISECONDS);
+
+
+       assertThat(receivedOpenReadyState.get()).isEqualTo(ReadyState.OPEN);
+
+       assertThat(receivedPayload.get()).isEqualTo("Hello");
+
+       assertThat(receivedCloseReadyState.get()).isEqualTo(ReadyState.CLOSED);
+       assertThat(receivedCloseCode.get()).isEqualTo(1000);
     }
 
     @Test
@@ -130,6 +166,4 @@ public abstract class AbstractSimpleClientTest {
 
         spc.connect();
     }
-
-
 }
